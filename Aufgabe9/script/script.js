3508ad9d88a9833b774289f74bd4b445046c4b2b
hannahdue <56439564+hannahdue@users.noreply.github.com>
2020-01-05 15:31:28 +0100

    aufgabe9 versuch1

--- Aufgabe9/script/script.js
+++ Aufgabe9/script/script.js
@@ -1,6 +1,5 @@
-var toDos = ["Kochen", "Putzen", "Training"];
+var toDos = ["Kochen", "Putzen"];
 window.addEventListener("load", function () {
-    document.querySelector("#tasknumber").innerHTML = "" + toDos.length;
     toDoList();
     var input = document.querySelector("#input");
     document.querySelector("#add").addEventListener("click", function () {
@@ -9,11 +8,36 @@ window.addEventListener("load", function () {
         console.log("huhu");
         toDoList();
     });
+    var checkButton = document.querySelector("#check");
+    function taskCheck() {
+        if (checkButton.getAttribute("style") == "opacity: 1") {
+            checkButton.setAttribute("style", "opacity: 0");
+        }
+        else {
+            checkButton.setAttribute("style", "opacity: 1");
+        }
+    }
+    document.querySelector("#check").addEventListener("click", function () { taskCheck(); });
+    document.querySelector("#trash").addEventListener("click", function () { deleteTask(); });
 });
+var checkButton = document.querySelector("#check");
+function taskCheck() {
+    if (checkButton.getAttribute("style") == "opacity: 1") {
+        checkButton.setAttribute("style", "opacity: 0");
+    }
+    else {
+        checkButton.setAttribute("style", "opacity: 1");
+    }
+}
 function toDoList() {
     document.querySelector("#todos").innerHTML = "";
     for (var index = 0; index < toDos.length; index++) {
-        document.querySelector("#todos").innerHTML += "<p>" + toDos[index] + "</p>";
+        document.querySelector("#todos").innerHTML += "<p>" + "<i class='far fa-circle'></i>" + "<i class='far fa-check' id='check'></i>" + toDos[index] + "<i class = 'far fa-trash-alt' id = 'trash'></i>" + "</p>";
+        taskCheck();
     }
+    document.querySelector("#tasknumber").innerHTML = "" + toDos.length;
+}
+function deleteTask() {
+    console.log("löschen");
 }
 //# sourceMappingURL=script.js.map
--- Aufgabe9/script/script.ts
+++ Aufgabe9/script/script.ts
@@ -1,9 +1,7 @@
 
-var toDos: string [] = ["Kochen", "Putzen", "Training"];
+var toDos: string [] = ["Kochen", "Putzen"];
 
 window.addEventListener("load", function(): void {
-
-    document.querySelector("#tasknumber").innerHTML = "" + toDos.length;
 
     toDoList();
    
@@ -16,11 +14,49 @@ window.addEventListener("load", function(): void {
             toDoList();
     });
 
+
+    var checkButton: HTMLElement = document.querySelector("#check");
+    function taskCheck(): void {
+
+        if (checkButton.getAttribute("style") == "opacity: 1") {
+            checkButton.setAttribute("style", "opacity: 0");
+        } else {
+            checkButton.setAttribute("style", "opacity: 1");
+        }
+
+    }    
+
+    document.querySelector("#check").addEventListener("click", function(): void {taskCheck(); });
+
+    document.querySelector("#trash").addEventListener("click", function(): void {deleteTask(); });
 });
 
-function toDoList(): void {
-    document.querySelector("#todos").innerHTML = "";
-    for (var index: number = 0; index < toDos.length; index++) {
-        document.querySelector("#todos").innerHTML += "<p>" + toDos[index] + "</p>";
+
+var checkButton: HTMLElement = document.querySelector("#check");
+function taskCheck(): void {
+
+    if (checkButton.getAttribute("style") == "opacity: 1") {
+        checkButton.setAttribute("style", "opacity: 0");
+    } else {
+        checkButton.setAttribute("style", "opacity: 1");
     }
 }
+
+    
+
+function toDoList(): void {
+
+    document.querySelector("#todos").innerHTML = "";
+    for (var index: number = 0; index < toDos.length; index++) {
+        document.querySelector("#todos").innerHTML += "<p>" + "<i class='far fa-circle'></i>" + "<i class='far fa-check' id='check'></i>" + toDos[index] + "<i class = 'far fa-trash-alt' id = 'trash'></i>" + "</p>";
+        taskCheck();
+    }
+    document.querySelector("#tasknumber").innerHTML = "" + toDos.length;
+
+}
+
+function deleteTask(): void {
+    console.log("löschen");
+}
+
+
