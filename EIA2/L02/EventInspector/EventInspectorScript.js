f1d1dd3ab91e9076c4d5896403b7a3e05d137c1f
hannahdue <56439564+hannahdue@users.noreply.github.com>
2021-04-13 17:18:13 +0200

    events exercise

--- EIA2/L02/EventInspector/EventInspectorScript.js
+++ EIA2/L02/EventInspector/EventInspectorScript.js
@@ -1,3 +1,46 @@
 "use strict";
-console.log("Skript verknüpft");
+console.log("Script connected");
+var EventInspector;
+(function (EventInspector) {
+    //let div0: HTMLDivElement = <HTMLDivElement>document.querySelector("#div0");
+    //let div1: HTMLDivElement = <HTMLDivElement>document.querySelector("#div1");
+    //let body: HTMLElement = <HTMLElement>document.querySelector("body");
+    let span = document.querySelector("#span");
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        console.log("handleLoad executed");
+        document.addEventListener("mousemove", setInfoBox);
+        //div0.addEventListener("mousemove", setInfoBox);
+        //div1.addEventListener("mousemove", setInfoBox);
+        //body.addEventListener("mousemove", setInfoBox);
+        document.addEventListener("click", logInfo);
+        //div0.addEventListener("click", logInfo);
+        //div1.addEventListener("click", logInfo);
+        //body.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
+        //div0.addEventListener("keyup", logInfo);
+        //div1.addEventListener("keyup", logInfo);
+        //body.addEventListener("keyup", logInfo);
+    }
+    function setInfoBox(_mouseevent) {
+        let top = _mouseevent.clientY + 10;
+        let left = _mouseevent.clientX + 10;
+        let mouseTarget = _mouseevent.target;
+        span.style.left = left + "px";
+        span.style.top = top + "px";
+        span.innerHTML = "<b>Current Position:</b><br> Top = " + top + "px | Left = " + left + " px<br>Target: " + mouseTarget;
+    }
+    function logInfo(_event) {
+        let eventType = _event.type;
+        let eventTarget = _event.target;
+        let currentTarget = _event.currentTarget;
+        let wholeEvent = _event;
+        console.group();
+        console.log("Event-Type: " + eventType);
+        console.log("Event-Target: " + eventTarget);
+        console.log("Current Target: " + currentTarget);
+        console.log("Whole Event: " + wholeEvent);
+        console.groupEnd();
+    }
+})(EventInspector || (EventInspector = {}));
 //# sourceMappingURL=EventInspectorScript.js.map
--- EIA2/L02/EventInspector/EventInspectorScript.ts
+++ EIA2/L02/EventInspector/EventInspectorScript.ts
@@ -1,24 +1,60 @@
-console.log("Skript verknüpft");
+console.log("Script connected");
 
 namespace EventInspector {
 
-    /*window.addEventListener("load", handleLoad);
+    //let div0: HTMLDivElement = <HTMLDivElement>document.querySelector("#div0");
+    //let div1: HTMLDivElement = <HTMLDivElement>document.querySelector("#div1");
+    //let body: HTMLElement = <HTMLElement>document.querySelector("body");
+    let span: HTMLSpanElement = <HTMLSpanElement>document.querySelector("#span");
 
-    function handleLoad(_event: MouseEvent) {
-        console.log("handleLoad ausgeführt");
+    window.addEventListener("load", handleLoad);
 
-        document.addEventListener("MouseMove", setInfoBox)
-        document.addEventListener("click", logInfo)
-        document.addEventListener("keyup", logInfo)
+    function handleLoad(): void {
+        console.log("handleLoad executed");
+
+        document.addEventListener("mousemove", setInfoBox);
+        //div0.addEventListener("mousemove", setInfoBox);
+        //div1.addEventListener("mousemove", setInfoBox);
+        //body.addEventListener("mousemove", setInfoBox);
+
+        document.addEventListener("click", logInfo);
+        //div0.addEventListener("click", logInfo);
+        //div1.addEventListener("click", logInfo);
+        //body.addEventListener("click", logInfo);
+
+        document.addEventListener("keyup", logInfo);
+        //div0.addEventListener("keyup", logInfo);
+        //div1.addEventListener("keyup", logInfo);
+        //body.addEventListener("keyup", logInfo);
         
     }
 
-    function setInfoBox(_event: MouseEvent) {
-        console.log("mouse moved");
+    function setInfoBox(_mouseevent: MouseEvent): void {
+        
+        let top: number = _mouseevent.clientY + 10;
+        let left: number = _mouseevent.clientX + 10;
+        let mouseTarget: EventTarget = <EventTarget>_mouseevent.target;
+
+        span.style.left = left + "px";
+        span.style.top = top + "px";
+
+        span.innerHTML = "<b>Current Position:</b><br> Top = " + top + "px | Left = " + left + " px<br>Target: " + mouseTarget;
+
     }
 
-    function logInfo(_event: Event) {
-        console.log("log info");
-    }*/
+    function logInfo(_event: Event): void {
+
+        let eventType: string = _event.type;
+        let eventTarget: EventTarget = <EventTarget>_event.target;
+        let currentTarget: EventTarget = <EventTarget>_event.currentTarget;
+        let wholeEvent: Event = <Event>_event;
+
+        console.group();
+        console.log("Event-Type: " + eventType);
+        console.log("Event-Target: " + eventTarget);
+        console.log("Current Target: " + currentTarget);
+        console.log("Whole Event: " + wholeEvent);
+        console.groupEnd();
+    }
 
 }
